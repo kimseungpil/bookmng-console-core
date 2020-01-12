366c8599334bd8319c4119a2780a8c606e01e246
Kim seungpil <seungpil80@gmail.com>
2020-01-12 12:09:37 +0900

    초기 커밋.

---
+++ bookmng-console-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bookmng_console_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
